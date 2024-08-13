3301fd72c77f665ee8633944eceae10b7d074594
cla6shade <cla6shade@github.com>
2024-08-14 08:47:21 +0900

    design: 업로드 폼 수정

--- src/pages/Upload/index.tsx
+++ src/pages/Upload/index.tsx
@@ -1,21 +1,26 @@
 import Page from '@components/templates/Page';
 import Input from '@components/atoms/Input';
 import Textarea from '@components/atoms/Textarea';
+import Container from '@components/atoms/Container';
 
 function UploadPage() {
   return (
     <Page pageName="영상 업로드" hideNavigation hideFooter>
-      <p>upload</p>
-      <Input
-        placeholder="30자 이내의 제목을 입력해 주세요."
-        maxLength={30}
-        type="text"
-      />
-      <Textarea
-        placeholder="100자 이내의 설명을 입력해 주세요."
-        maxLength={100}
-        rows={4}
-      />
+      <Container size="full-width">
+        <Container size="full-width" classes="px-3 gap-3" direction="flex-col">
+          <p>upload</p>
+          <Input
+            placeholder="30자 이내의 제목을 입력해 주세요."
+            maxLength={30}
+            type="text"
+          />
+          <Textarea
+            placeholder="100자 이내의 설명을 입력해 주세요."
+            maxLength={100}
+            rows={4}
+          />
+        </Container>
+      </Container>
     </Page>
   );
 }
