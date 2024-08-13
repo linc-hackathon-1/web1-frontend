936c6bcc32730ba6b775d975234e100c949e3b7a
cla6shade <cla6shade@github.com>
2024-08-14 04:43:11 +0900

    feat: Container가 div attribute를 받을 수 있도록 수정

--- src/components/atoms/Container.tsx
+++ src/components/atoms/Container.tsx
@@ -1,9 +1,9 @@
-import { ComponentProps, ReactNode } from 'react';
+import { ComponentProps, HTMLAttributes, ReactNode } from 'react';
 import { twMerge } from 'tailwind-merge';
 import { convertSizeToClass } from '@/utils';
 import { ContainerSize } from '@/types';
 
-interface ContainerProps {
+interface ContainerProps extends HTMLAttributes<HTMLDivElement> {
   children?: ReactNode;
   size?: ContainerSize;
   classes?: ComponentProps<'div'>['className'];
@@ -13,13 +13,13 @@ interface ContainerProps {
 }
 
 function Container({
-  children, size = 'auto', classes, direction = 'flex-row', alignItems, justifyContents,
+  children, size = 'auto', classes, direction = 'flex-row', alignItems, justifyContents, ...rest
 }: ContainerProps) {
   const sizeClass = convertSizeToClass(size);
   const merged = twMerge('flex', sizeClass, classes, direction, alignItems, justifyContents);
 
   return (
-    <div className={merged}>
+    <div className={merged} {...rest}>
       {children}
     </div>
   );
