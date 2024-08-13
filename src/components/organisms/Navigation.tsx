4fb51f4c54dc548343668155699dcc33fa472e2b
cla6shade <cla6shade@github.com>
2024-08-14 03:31:26 +0900

    refactor: 하드코딩된 인수 재작성

--- src/components/organisms/Navigation.tsx
+++ src/components/organisms/Navigation.tsx
@@ -4,8 +4,11 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import Container from '@/components/atoms/Container';
+import { RouterPath } from '@/routes/path';
 
 function Navigation() {
+  const DEFAULT_VIDEO_ID = 'defaultVideoID';
+
   return (
     <Container
       size="full-width"
@@ -14,22 +17,22 @@ function Navigation() {
       justifyContents="justify-around"
       classes="fixed bottom-0 left-0 bg-white p-3 "
     >
-      <Link to="/" className="flex flex-col items-center gap-1">
+      <Link to={RouterPath.root} className="flex flex-col items-center gap-1">
         <FontAwesomeIcon className="text-[20px]" icon={faHome} />
         <div className="text-[8px]">홈</div>
       </Link>
 
-      <Link to="/ranking" className="flex flex-col items-center gap-1">
+      <Link to={RouterPath.ranking} className="flex flex-col items-center gap-1">
         <FontAwesomeIcon className="text-[20px]" icon={faRankingStar} />
         <div className="text-[8px]">랭킹</div>
       </Link>
 
-      <Link to="/videos" className="flex flex-col items-center gap-1">
+      <Link to={RouterPath.video(DEFAULT_VIDEO_ID)} className="flex flex-col items-center gap-1">
         <FontAwesomeIcon className="text-[20px]" icon={faVideo} />
         <div className="text-[8px]">영상</div>
       </Link>
 
-      <Link to="/upload" className="flex flex-col items-center gap-1">
+      <Link to={RouterPath.upload} className="flex flex-col items-center gap-1">
         <FontAwesomeIcon className="text-[20px]" icon={faFileVideo} />
         <div className="text-[8px]">업로드</div>
       </Link>
