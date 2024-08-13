907013d613a5640b6d37b581288f087ec15e64e7
uicheol <suc2150@gmail.com>
2024-08-13 17:36:07 +0900

    temp

---
+++ src/components/organisms/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = ({ pageName }) => {
+  return (
+    <>
+      <div>
+        <div>{pageName}</div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
