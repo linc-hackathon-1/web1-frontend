1de2cee2175c18a8f2b7e0ed22c073d4e4d17df5
cla6shade <cla6shade@github.com>
2024-08-14 06:47:36 +0900

    feat: 뒤로가기 버튼

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,15 +1,30 @@
 import Container from '@components/atoms/Container';
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { RouterPath } from '@/routes/path';
 
 type HeaderProps = {
   pageName: string;
 };
 
 function Header({ pageName }: HeaderProps) {
+  const navigate = useNavigate();
+
+  const handleClick = useCallback(() => {
+    if (pageName === '영상') {
+      navigate(RouterPath.home);
+
+      return;
+    }
+
+    navigate(-1);
+  }, [pageName, navigate]);
+
   return (
     <header>
       <Container classes="h-[57px]">
         {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button className="text-gray-700 pl-4 cursor-pointer" type="button">
+        <button className="text-gray-700 pl-4 cursor-pointer" type="button" onClick={handleClick}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
