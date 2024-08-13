869cb5626ae22a681b2fedf784368cc616d35269
harugi7 <gimme2721@gmail.com>
2024-08-14 06:32:54 +0900

    feat: home page의 banner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,5 @@
+import Button from '@/components/atoms/Button';
+import Container from '@/components/atoms/Container';
 import Profile from '@/components/organisms/Profile';
 import Page from '@/components/templates/Page';
 
@@ -5,7 +7,19 @@ function HomePage() {
   return (
     <Page pageName="Home" hideFooter>
       <Profile bgImg="https://picsum.photos/200" profileImg="https://picsum.photos/200" title="title" description="tjfaud" totalView={123} likeNum={1200} />
-      <p>test</p>
+      <Container
+        style={{ background: 'linear-gradient(120deg, #F7A6A6, #FD7070, #F29539, #FAC60E)' }}
+        direction="flex-col"
+        classes="h-95px pt-[16px] mx-[25px] mt-[13px] rounded-lg"
+      >
+        <div className="ml-[18px] text-white font-bold">
+          <p>우리 지역을</p>
+          <p>가장 쉽게 알리는 방법은?</p>
+        </div>
+        <div className="flex justify-end mt-[13px] shadow-lg">
+          <Button bgColor="bg-black" classes="mx-[14px] mb-[8px] w-[125px] h-[25px] text-[10px] flex items-center justify-center">비용 없이 홍보하기 &gt;</Button>
+        </div>
+      </Container>
     </Page>
   );
 }
