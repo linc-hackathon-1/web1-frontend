400b893ab292f808f3e5d629e60a0bf09eb5314c
cla6shade <cla6shade@github.com>
2024-08-13 23:23:36 +0900

    refactor: constant 분리
    관심사에 따라 endpoint가 유틸 모듈 옆에 있으면 좋을 것 같음.

--- src/constants/index.ts
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export const endpoints = {
-  UPLOAD_VIDEO: (provinceId: string) => `/api/video/${provinceId}`,
-  GET_VIDEO: (videoId: string) => `/api/video/${videoId}`,
-};
