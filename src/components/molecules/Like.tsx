7e81c8533842e6e4a1afa141eb39e40b021d0140
cla6shade <cla6shade@github.com>
2024-08-14 06:47:55 +0900

    feat: 좋아요 버튼 variation

--- src/components/molecules/Like.tsx
+++ src/components/molecules/Like.tsx
@@ -1,16 +1,26 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { twMerge } from 'tailwind-merge';
 import Container from '@/components/atoms/Container';
 
 type LikeProps = {
   LikeNum: number;
+  size?: string;
+  captionColor?: 'text-white' | 'text-grey';
 };
 
-function Like({ LikeNum }: LikeProps) {
+function Like({ LikeNum, size, captionColor }: LikeProps) {
+  let sizeClass = 'size-[14px]';
+  const mergedTextClass = twMerge('text-12px', captionColor);
+
+  if (size) {
+    sizeClass = size;
+  }
+
   return (
     <Container direction="flex-col" alignItems="items-center" justifyContents="justify-center">
-      <FontAwesomeIcon icon={faHeart} style={{ color: 'red' }} className="size-[14px]" />
-      <p className="text-[12px] text-grey">
+      <FontAwesomeIcon icon={faHeart} style={{ color: 'red' }} className={sizeClass} />
+      <p className={mergedTextClass}>
         {LikeNum}
       </p>
     </Container>
