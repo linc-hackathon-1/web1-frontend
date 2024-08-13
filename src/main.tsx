915b8eaa28d543ae2e354dae63186a823fa9fb53
cla6shade <cla6shade@github.com>
2024-08-13 21:17:41 +0900

    refactor: container atom을 사용하여 리팩토링

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import Header from "./components/organisms/Header.tsx";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Header from '@components/organisms/Header';
+import './index.css';
+import Footer from '@components/organisms/Footer';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Header pageName={"지자체 상세보기"} />
-  </StrictMode>
+    <Header pageName="지자체 상세보기" />
+    <Footer />
+  </StrictMode>,
 );
