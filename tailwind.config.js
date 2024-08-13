6bbf391947c47eab20956ef7043f1a8017f02170
harugi7 <gimme2721@gmail.com>
2024-08-13 18:10:22 +0900

    chore: tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ export default {
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
+    colors: {
+      'primary': '#FF5566',
+      'primaryDark': '#EC0039',
+      'black': '#2C1E1E',
+      'darkGrey': '#444444',
+      'grey': '#747474',
+      'lightGrey': '#CCCCCC',
+      'lighterGrey': '#F4F4F4',
+      'white': '#FCFCFC',
+    },
+    fontFamily: {
+      pretendard: ['Pretendard Variable', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
